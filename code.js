2e85c5d7fe578e6bbc42aae0ec9020b44ef010c8
Delta-Water <3627371741@qq.com>
2025-12-14 11:31:48 +0800

    更新头文件以适配OUJ

--- code.js
+++ code.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name           解除粘贴限制
 // @namespace      https://github.com/Delta-Water
-// @version        2.0.0
+// @version        2.0.1
 // @description    允许在CQUPT内网上的代码平台上粘贴文本并通过平台的按键检测
 // @author         Delta_Water
 // @match          *://172.22.181.82/train/*
 // @grant          GM_setValue
 // @grant          GM_getValue
 // @grant          GM_addStyle
-// @license        AGPL-3.0
+// @license        AGPL-3.0-or-later
 // @run-at         document-idle
+// @updateURL      https://raw.githubusercontent.com/Delta-Water/allow-CTRL-V/refs/heads/main/code.js
 // ==/UserScript==
 
 // ==/UserScript==

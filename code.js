cf27ca546832c402c57721647602c3702d2177a0
Delta-Water <3627371741@qq.com>
2025-12-14 11:05:03 +0800

    更新命名空间

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           解除粘贴限制
-// @namespace      http://tampermonkey.net/
+// @namespace      https://github.com/Delta-Water
 // @version        2.0.0
 // @description    允许在CQUPT内网上的代码平台上粘贴文本并通过平台的按键检测
 // @author         Delta_Water
